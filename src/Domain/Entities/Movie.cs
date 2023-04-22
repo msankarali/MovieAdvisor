087ec5bb9e72ba53d92ef940a9c2bcc90e4d767a
M. Servet ANKARALI <msankarali@outlook.com>
2023-04-22 06:50:02 +0300

    del null check

--- src/Domain/Entities/Movie.cs
+++ src/Domain/Entities/Movie.cs
@@ -6,6 +6,6 @@ public class Movie : BaseEntity
     public string Title { get; set; }
     public string Description { get; set; }
     public DateTime ReleaseDate { get; set; }
-    
-    public virtual List<Rating>? Ratings { get; set; }
+
+    public virtual List<Rating> Ratings { get; set; }
 }
