0e170148bf54831f0e2d4bbf5d12ea521defce7c
M. Servet ANKARALI <msankarali@outlook.com>
2023-04-22 07:10:06 +0300

    add base controller and mediatr implementation

--- src/WebApi/Controllers/BaseController.cs
+++ src/WebApi/Controllers/BaseController.cs
@@ -1,10 +1,13 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class BaseController : ControllerBase
 {
-    
+    private ISender? _mediator;
+
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
 }
