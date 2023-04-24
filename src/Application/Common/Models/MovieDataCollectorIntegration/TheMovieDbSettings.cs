4ad6c2578f45ce1be09865c72ab3311dc6cdc158
M. Servet ANKARALI <msankarali@outlook.com>
2023-04-24 07:57:45 +0300

    add TheMovieDb integration models and settings

---
+++ src/Application/Common/Models/MovieDataCollectorIntegration/TheMovieDbSettings.cs
@@ -0,0 +1,7 @@
+namespace Application.Common.Models.MovieDataCollectorIntegration;
+
+public class TheMovieDbSettings
+{
+    public string APIKey_v3 { get; set; }
+    public string APIReadToken_v4 { get; set; }
+}
