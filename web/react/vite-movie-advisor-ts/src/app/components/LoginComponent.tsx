0f40beccf810535822f3eee03584759edccb6cd3
M. Servet ANKARALI <msankarali@outlook.com>
2023-05-01 21:55:59 +0300

    add register login modal components

--- web/react/vite-movie-advisor-ts/src/app/components/LoginComponent.tsx
+++ web/react/vite-movie-advisor-ts/src/app/components/LoginComponent.tsx
@@ -1,26 +1,21 @@
 import { observer } from "mobx-react-lite";
 import { useStore } from "../stores/store";
+import RegisterComponent from "./RegisterComponent";
 
 const LoginComponent = () => {
 
     const { userStore, modalStore } = useStore();
 
     return (
-        <>
-            crazy sample here. I have this data here: {userStore.someData}
-            <button onClick={() => modalStore.open({
-                name: "somethinn",
-                body: <>
-                    <p>
-                        Another modal here!!!
-                    </p>
-                    <p>
-                        <button onClick={() => { userStore.someData = "YES CRAZYYYY!" }}>CLICK HERE!</button>
-                    </p>
-                    <p>and manipulate the data...</p>
-                </>
-            })}>open another modal</button>
-        </>
+        <div className="center">
+            <div style={{ textAlign: 'center', fontWeight: 100, fontSize: '28px' }}>WELCOME</div><br />
+            <input value={userStore.email} type="text" placeholder="Email" onChange={x => userStore.email = x.target.value}></input><br /><br />
+            <input value={userStore.password} type="password" placeholder="Password" onChange={x => userStore.password = x.target.value}></input><br /><br />
+            <button className="btn" style={{ width: '100%', fontWeight: 200 }} onClick={async () => userStore.login({ email: userStore.email, password: userStore.password })}>Login</button><br /><br />
+            <button className="btn" style={{ width: '100%', fontWeight: 200 }} onClick={x => {
+                modalStore.open({ name: "registerPage", body: <RegisterComponent /> });
+            }}>Register</button>
+        </div >
     );
 }
 
--- web/react/vite-movie-advisor-ts/src/app/components/RegisterComponent.tsx
+++ web/react/vite-movie-advisor-ts/src/app/components/RegisterComponent.tsx
@@ -1,9 +1,22 @@
+import { observer } from "mobx-react-lite";
+import { useStore } from "../stores/store";
+
 const RegisterComponent = () => {
+
+    const { userStore, modalStore } = useStore();
+
     return (
-        <>
-            
-        </>
-    )
+        <div className="center">
+            <div style={{ textAlign: 'center', fontWeight: 100, fontSize: '28px' }}>BE ONE OF US</div><br />
+            <input type="text" placeholder="First name" onChange={x => userStore.firstName = x.target.value}></input><br /><br />
+            <input type="text" placeholder="Last name" onChange={x => userStore.lastName = x.target.value}></input><br /><br />
+            <input value={userStore.email} type="text" placeholder="Email" onChange={x => userStore.email = x.target.value}></input><br /><br />
+            <input value={userStore.password} type="password" placeholder="Password" onChange={x => userStore.password = x.target.value}></input><br /><br />
+            <button className="btn" style={{ width: '100%', fontWeight: 200 }} onClick={async () => {
+                await userStore.register({ email: userStore.email, firstName: userStore.firstName, lastName: userStore.lastName, password: userStore.password })
+            }}>Register</button>
+        </div >
+    );
 }
 
-export default RegisterComponent;
+export default observer(RegisterComponent);
