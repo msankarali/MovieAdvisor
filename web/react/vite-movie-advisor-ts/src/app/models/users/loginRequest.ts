a264adac755f6d8b9410a4c8e21885063ef157b0
M. Servet ANKARALI <msankarali@outlook.com>
2023-05-01 21:52:30 +0300

    add login register models

--- web/react/vite-movie-advisor-ts/src/app/models/users/loginRequest.ts
+++ web/react/vite-movie-advisor-ts/src/app/models/users/loginRequest.ts
@@ -1,4 +1,20 @@
-export default interface LoginRequest {
-    email: string;
-    password: string;
+export interface LoginRequest {
+    email?: string;
+    password?: string;
+}
+
+export class LoginRequestFormValues {
+    email?: string;
+    password?: string;
+
+    constructor(loginRequestFormValues?: LoginRequestFormValues) {
+        this.email = loginRequestFormValues?.email;
+        this.password = loginRequestFormValues?.password;
+    }
+}
+
+export class LoginRequest implements LoginRequest {
+    constructor(init?: LoginRequestFormValues) {
+        Object.assign(this, init);
+    }
 }
--- web/react/vite-movie-advisor-ts/src/app/models/users/registerRequest.ts
+++ web/react/vite-movie-advisor-ts/src/app/models/users/registerRequest.ts
@@ -1,6 +1,21 @@
 export default interface RegisterRequest {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    email: string | undefined;
+    password: string | undefined;
+}
+
+
+export class RegisterRequestFormValues {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    password?: string;
+
+    constructor(registerRequestFormValues?: RegisterRequestFormValues) {
+        this.firstName = registerRequestFormValues?.firstName;
+        this.lastName = registerRequestFormValues?.lastName;
+        this.email = registerRequestFormValues?.email;
+        this.password = registerRequestFormValues?.password;
+    }
 }
